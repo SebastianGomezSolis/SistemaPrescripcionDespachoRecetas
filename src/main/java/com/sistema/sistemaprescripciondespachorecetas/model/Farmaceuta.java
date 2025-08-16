c7b8d91fa91b779ccc4f3c8d4ca80ebdf9958af8
SebastianGomez <sebasjose13@gmail.com>
2025-08-15 22:38:51 -0600

    Inclucion de clases bases

---
+++ src/main/java/com/sistema/sistemaprescripciondespachorecetas/model/Farmaceuta.java
@@ -0,0 +1,27 @@
+package com.sistema.sistemaprescripciondespachorecetas.model;
+
+public class Farmaceuta {
+    private String id;
+    private String clave;
+    private String nombre;
+
+    // Constructor sin parametros
+    public Farmaceuta() {}
+
+    // Constructor con parametros
+    public Farmaceuta(String id, String clave, String nombre) {
+        this.id = id;
+        this.clave = clave;
+        this.nombre = nombre;
+    }
+
+    // Getters y Setters
+    public String getId() { return id; }
+    public String getClave() { return clave; }
+    public String getNombre() { return nombre; }
+
+    public void setId(String id) { this.id = id; }
+    public void setClave(String clave) { this.clave = clave; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+}
